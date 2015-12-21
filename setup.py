c1e61d9446546e979b6a6929855f58853c3b0688
jasontrigg0 <jasontrigg0@gmail.com>
2015-12-20 23:40:27 -0500

    pypi version 0.0.1

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name = 'webgrep',
     packages = ['webgrep'],
-    version = "0.0.0",
+    version = "0.0.1",
     description = 'Data workflow tool (rough copy of drake for python)',
     author = "Jason Trigg",
     author_email = "jasontrigg0@gmail.com",
     url = "https://github.com/jasontrigg0/webgrep",
-    download_url = 'https://github.com/jasontrigg0/webgrep/tarball/0.0.0',
+    download_url = 'https://github.com/jasontrigg0/webgrep/tarball/0.0.1',
     scripts=[],
     install_requires=[
         "beautifulsoup4",
