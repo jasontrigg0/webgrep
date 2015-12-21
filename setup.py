553c8a95e26b625cdc160ae7b78cc627ab91fa54
jasontrigg0 <jasontrigg0@gmail.com>
2015-12-21 01:28:50 -0500

    pypi version 0.0.4

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name = 'webgrep',
     packages = ['webgrep'],
-    version = "0.0.3",
+    version = "0.0.4",
     description = 'Command line website scraper',
     author = "Jason Trigg",
     author_email = "jasontrigg0@gmail.com",
     url = "https://github.com/jasontrigg0/webgrep",
-    download_url = 'https://github.com/jasontrigg0/webgrep/tarball/0.0.3',
+    download_url = 'https://github.com/jasontrigg0/webgrep/tarball/0.0.4',
     scripts=["webgrep/webgrep.py"],
     install_requires=[
         "beautifulsoup4",
