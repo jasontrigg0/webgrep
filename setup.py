ad48f99ddddde94a898734b7e1423a63969c55f0
jasontrigg0 <jasontrigg0@gmail.com>
2015-12-21 01:17:02 -0500

    add script to setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email = "jasontrigg0@gmail.com",
     url = "https://github.com/jasontrigg0/webgrep",
     download_url = 'https://github.com/jasontrigg0/webgrep/tarball/0.0.2',
-    scripts=[],
+    scripts=["webgrep/webgrep.py"],
     install_requires=[
         "beautifulsoup4",
     ],
